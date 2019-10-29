f735302c74eafb73ef296d03687024431979a6d0
Tomas Carnecky <tomc@caurea.org>
2019-10-29 17:05:31 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as MUI from "@material-ui/core";
+
+ReactDOM.render(
+  <MUI.Box component="h1" mr={3} color="teal">
+    This should be teal
+  </MUI.Box>,
+  document.getElementById("root")
+);
